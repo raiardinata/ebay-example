7bd6091d97a17cf8ee769e00ac285123c41aaf4f
Parama Danoesubroto <parama@answersengine.com>
2018-11-28 01:10:56 -0500

    fix bug, that it should append to the ouput

--- parsers/details.rb
+++ parsers/details.rb
@@ -8,7 +8,7 @@ seller = nokogiri.at_css('.si-inner .mbg-nw')&.text
 feedback = nokogiri.at_css('.si-inner #si-fb')&.text
 
 # save it into outputs
-outputs = {
+outputs << {
     _collection: 'products',
     title: page['vars']['title'],
     price: page['vars']['price'],
