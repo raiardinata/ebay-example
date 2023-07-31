750ed36d2898b524d9e25fa5b60d0a45ab1ca2eb
IemmaJedi <59637649+raiardinata@users.noreply.github.com>
2023-07-31 20:06:35 +0700

    Update finisher.rb

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -5,3 +5,13 @@ outputs << {
     '_collection' => 'finisher',
     'message' => 'Exporters has been started!'
 }
+outputs << {
+  _collection: 'env_vars',
+  _id: 'finisher',
+  my_env_var: ENV['my_env_var'],
+  my_env_secret_var: ENV['my_env_secret_var'],
+  my_env_hardcode_var: ENV['my_env_hardcode_var'],
+  my_input_secret_var: ENV['my_input_secret_var'],
+  my_input_var: ENV['my_input_var'],
+  my_input_hardcode_var: ENV['my_input_hardcode_var']
+}
